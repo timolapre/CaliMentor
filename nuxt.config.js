841125ae65a6000263f4bfd767ee6d28489021f7
timolapre <timolapre1998@gmail.com>
2021-01-24 23:12:45 +0100

    added icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,5 +66,11 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    loaders: {
+      vue: {
+        prettify: false
+      }
+    }
+  },
 }
